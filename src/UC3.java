f89571ca5c9684a7aadc4067c33b367fadeda22f
FuhadSaninAP <fuhadsaninapfs@gmail.com>
2026-02-24 09:47:59 +0530

    UC3

---
+++ src/UC3.java
@@ -0,0 +1,25 @@
+// UseCase3PalindromeCheckerApp.java
+
+public class UC3 {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String original = "racecar";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // Compare original and reversed using equals()
+        if (original.equals(reversed)) {
+            System.out.println("The string \"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
+        }
+    }
+}
